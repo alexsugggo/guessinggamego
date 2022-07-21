773bef29cb17486c941d30ac9bacce2c74f978bd
Alexander Sugg <55547710+alexsugggo@users.noreply.github.com>
2022-07-21 10:15:39 -0500

    first version Go guessing game

---
+++ guessinggame.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var number = 40
+	for {
+		var n = rand.Intn(100) + 1
+		if n < number {
+			fmt.Println("%v is too small.\n", n)
+		} else if n > number {
+			fmt.Println("%v is too big.\n", n)
+		} else {
+			fmt.Println("You got it! %v\n", n)
+			break
+		}
+	}
+}
+
